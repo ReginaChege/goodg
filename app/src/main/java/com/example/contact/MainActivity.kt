cd997870b87eb09ea04c188ed0def9b7a5863528
ReginaChege <reginachegewairimu@gmail.com>
2023-05-31 23:38:15 +0300

    second

--- app/src/main/java/com/example/contact/MainActivity.kt
+++ app/src/main/java/com/example/contact/MainActivity.kt
@@ -18,14 +18,13 @@ override fun onResume() {
     displayContact()
 }
     fun displayContact(){
-        val name1=ContactData("Angeth","076564310","angethbecky@gmail.com","")
-        val name2=ContactData("John", "0788564370","johnmwangi@gmail.com","")
-        val name3=ContactData("Wani","0702364550","waniigga@gmail.com"," ")
-        val name4=ContactData("Herjok","0715364750","herjokmach@gmail.com"," ")
-        val name5=ContactData("Joy","0795365380","joymoses@gmail.com","")
+        val contact1=ContactData("Angeth","070987450","angeth@gmail.com","")
+        val contact3=ContactData("Wanifrida","078765340","wanifrida@gmail.com"," ")
+        val contact4=ContactData("Herjok","079874750","herjok@gmail.com"," ")
+        val contact5=ContactData("Joyce","079235380","joyce@gmail.com","")
 
 
-       val contactName=listOf(name1,name2,name3,name4,name5)
+       val contactName=listOf(contact1,contact3,contact4,contact5)
        val contactAdapter=ContactAdapter(contactName)
         binding.rvcontact.layoutManager=LinearLayoutManager(this)
         binding.rvcontact.adapter=contactAdapter
